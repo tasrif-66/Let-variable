94915d0c4946113f351b409dc5fec6954cf81e4a
tasrif-66 <mtasrif20@gmail.com>
2025-05-12 12:17:35 +0600

    let variable practice

---
+++ let.js
@@ -0,0 +1,33 @@
+let age = 35;
+age = 20;
+console.log(age);
+
+let poketMoney = 500;
+poketMoney = 150;
+console.log(poketMoney);
+
+let studyTime = 4;
+studyTime = 2;
+studyTime = 9;
+console.log(studyTime);
+
+let frinendsCount = 8;
+frinendsCount = 5;
+console.log(frinendsCount);
+
+let phonePrice = 25000;
+phonePrice = 12000;
+console.log(phonePrice);
+
+let chairsCount = 40;
+chairsCount = 38;
+chairsCount = 32;
+console.log(chairsCount);
+
+let playTime = 3;
+playTime = 0;
+console.log(playTime);
+
+
+
+
